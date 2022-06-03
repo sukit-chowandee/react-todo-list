b9b10724b15216106bbd1037d19c8b093782e790
sukit-chowandee <sukitchowandee@gmail.com>
2022-06-03 22:09:41 +0700

    no message

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -54,7 +54,7 @@ function TodoList() {
 
   return (
     <div>
-      <h1>Todo List sdfdsfds</h1>
+      <h1>Todo List</h1>
       <TodoForm onSubmit={addTodo} />
       <Todo
         todos={todos}
